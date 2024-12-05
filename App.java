a6d5fdf6491de7ae7a45ce6131b044785e0d715b
Muskansingla565 <muskan2006.singla@gmail.com>
2024-12-05 17:05:59 +0530

    New file

---
+++ App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int myVariable1=10;
+        int my_variable2=30;
+        int $myvar=40;
+        System.out.println(myVariable1);
+        System.out.println(my_variable2);
+        System.out.println($myvar);
+    }
+}
---
+++ main.java
@@ -0,0 +1,35 @@
+// import java.util.Scanner;
+// public class main{
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int num = sc.nextInt();
+//         System.out.println(num);
+//     }
+// }
+
+
+
+// import java.util.Scanner;
+// public class main{
+//     public static int add(int a,int b){
+//         return a+b;
+//     }
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         int a = sc.nextInt();
+//         int b = sc.nextInt();
+//         int sum = add(a,b);
+//         System.out.println(sum);
+//     }
+// }
+
+
+
+public class main{
+    public static void main(String[] args){
+        int [] arr ={10,20,30,40,50};
+        for(int i=0;i<arr.length;i++){
+            System.out.println("Element at index "+ i +" is "+arr[i]);
+        }
+    }
+}
